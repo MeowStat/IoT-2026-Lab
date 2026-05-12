d0dd4e0f09942c9741ef8aec7b06116726d326ee
MeowStat <tinnie2004@gmail.com>
2026-05-12 07:38:49 +0700

    feat: setup main server

--- src/global.cpp
+++ src/global.cpp
@@ -2,15 +2,18 @@
 float glob_temperature = 0;
 float glob_humidity = 0;
 
+bool led1_state = false;
+bool led2_state = false;
+
 String WIFI_SSID;
 String WIFI_PASS;
-String CORE_IOT_TOKEN;
-String CORE_IOT_SERVER;
-String CORE_IOT_PORT;
+String CORE_IOT_TOKEN = "AnGdNEIQNZjoIejnBilZ";
+String CORE_IOT_SERVER = "app.coreiot.io";
+String CORE_IOT_PORT = "1883";
 
-String ssid = "ESP32-YOUR NETWORK HERE!!!";
+String ssid = "ESP32 2CE1CS";
 String password = "12345678";
-String wifi_ssid = "abcde";
-String wifi_password = "123456789";
+String wifi_ssid;
+String wifi_password;
 boolean isWifiConnected = false;
 SemaphoreHandle_t xBinarySemaphoreInternet = xSemaphoreCreateBinary();
